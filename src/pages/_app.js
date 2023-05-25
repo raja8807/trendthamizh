13b180a20907f264d6dcd03ddabc9e8b82c0c70f
Raja <s.rajarathinam1999@gmail.com>
2023-05-25 16:54:15 +0530

    ga disabled temproarily

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,17 +4,7 @@ import Script from "next/script";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {/* <Script
-        id=""
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-KBP0JT1RB7"
-      >
-        {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-KBP0JT1RB7');
-       `}
-      </Script> */}
+{/*      
        <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-KBP0JT1RB7"
         strategy="afterInteractive"
@@ -27,7 +17,7 @@ export default function App({ Component, pageProps }) {
 
           gtag('config', 'G-KBP0JT1RB7');
         `}
-      </Script>
+      </Script> */}
       <Component {...pageProps} />
     </>
   );
